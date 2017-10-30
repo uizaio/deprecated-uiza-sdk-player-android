b0a4509f9302998f993649ec54cc36767dcb72dc
Loitp <loitp@pateco.vn>
2017-10-30 09:51:56 +0700

    update

--- app/src/main/java/vn/loitp/app/activity/demo/video/videodemo5/VideoDemo5Activity.java
+++ app/src/main/java/vn/loitp/app/activity/demo/video/videodemo5/VideoDemo5Activity.java
@@ -8,8 +8,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import loitp.utils.util.ToastUtils;
 import vn.loitp.app.activity.demo.video.videodemo3.lib.helper.InputModel;
-import vn.loitp.app.activity.demo.video.videodemo4.frm.FrmUizaVideo;
+import vn.loitp.app.activity.demo.video.videodemo5.frm.FrmUizaVideo;
 import vn.loitp.app.activity.demo.video.videodemo5.model.Sample;
 import vn.loitp.app.app.LSApplication;
 import vn.loitp.app.base.BaseActivity;
@@ -23,25 +24,32 @@ public class VideoDemo5Activity extends BaseActivity {
         super.onCreate(savedInstanceState);
         orientVideoDescriptionFragment(getResources().getConfiguration().orientation);
 
+        //TODO replace sample to inputmodel
         Sample sample = (Sample) getIntent().getSerializableExtra(Constants.KEY_UIZA);
+        LLog.d(TAG, "sample " + LSApplication.getInstance().getGson().toJson(sample));
         if (sample != null) {
-            LLog.d(TAG, "sample " + LSApplication.getInstance().getGson().toJson(sample));
-        }
+            InputModel inputModel = new InputModel();
+            inputModel.setUri(Uri.parse(sample.getUri()));
+            inputModel.setDrmLicenseUrl(sample.getDrmLicenseUrl());
+            inputModel.setAction("com.google.android.exoplayer.demo.action.VIEW");
+            inputModel.setAdTagUri(sample.getAdTagUri());
+            inputModel.setPreferExtensionDecoders(false);
 
-        //TODO
-        InputModel inputModel = new InputModel();
-        inputModel.setUri(Uri.parse("http://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"));
-        inputModel.setDrmLicenseUrl("https://proxy.uat.widevine.com/proxy?video_id\\u003dd286538032258a1c\\u0026provider\\u003dwidevine_test");
-        inputModel.setAction("com.google.android.exoplayer.demo.action.VIEW");
-        inputModel.setAdTagUri("https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=");
-        inputModel.setPreferExtensionDecoders(false);
+            /*inputModel.setUri(Uri.parse("http://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"));
+            inputModel.setDrmLicenseUrl("https://proxy.uat.widevine.com/proxy?video_id\\u003dd286538032258a1c\\u0026provider\\u003dwidevine_test");
+            inputModel.setAction("com.google.android.exoplayer.demo.action.VIEW");
+            inputModel.setAdTagUri("https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator=");
+            inputModel.setPreferExtensionDecoders(false);*/
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment frmUizaVideo = fragmentManager.findFragmentById(R.id.uiza_video);
-        if (frmUizaVideo != null) {
-            if (frmUizaVideo instanceof FrmUizaVideo) {
-                ((FrmUizaVideo) frmUizaVideo).setInputModel(inputModel, true);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment frmUizaVideo = fragmentManager.findFragmentById(R.id.uiza_video);
+            if (frmUizaVideo != null) {
+                if (frmUizaVideo instanceof FrmUizaVideo) {
+                    ((FrmUizaVideo) frmUizaVideo).setInputModel(inputModel, true);
+                }
             }
+        } else {
+            ToastUtils.showShort("Error: sample is null");
         }
     }
 
