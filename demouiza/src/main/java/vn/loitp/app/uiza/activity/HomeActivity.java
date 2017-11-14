f323f8a146451efca62645ccd4a55b5b3920d610
Loitp <loitp@pateco.vn>
2017-11-14 13:38:01 +0700

    update home screen

--- demouiza/src/main/java/vn/loitp/app/uiza/activity/HomeActivity.java
+++ demouiza/src/main/java/vn/loitp/app/uiza/activity/HomeActivity.java
@@ -2,16 +2,38 @@ package vn.loitp.app.uiza.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
+import vn.loitp.app.activity.customviews.placeholderview._lib.placeholderview.PlaceHolderView;
+import vn.loitp.app.activity.customviews.placeholderview.ex.androidnavigationdrawer.DrawerHeader;
+import vn.loitp.app.activity.customviews.placeholderview.ex.androidnavigationdrawer.DrawerMenuItem;
 import vn.loitp.app.base.BaseActivity;
+import vn.loitp.app.utilities.LStoreUtil;
+import vn.loitp.app.utilities.LUIUtil;
 import vn.loitp.livestar.R;
 
 public class HomeActivity extends BaseActivity {
+    private PlaceHolderView mDrawerView;
+    private DrawerLayout mDrawer;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerView = (PlaceHolderView) findViewById(R.id.drawerView);
 
+        LUIUtil.setPullLikeIOSVertical(mDrawerView);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setupDrawer();
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(LStoreUtil.readTxtFromRawFolder(activity, R.raw.loitp));
     }
 
     @Override
@@ -32,5 +54,33 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.uiza_home_activity;
+    }
+
+    private void setupDrawer() {
+        mDrawerView
+                .addView(new DrawerHeader())
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()))
+                .addView(new DrawerMenuItem(this.getApplicationContext()));
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        mDrawer.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 }
