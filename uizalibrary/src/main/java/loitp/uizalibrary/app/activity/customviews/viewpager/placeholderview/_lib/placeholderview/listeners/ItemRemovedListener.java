2d9c99a273fb10bebe3d4fb0641a1bd59032a985
Loitp <loitp@pateco.vn>
2017-11-17 13:32:28 +0700

    update home screen

--- uizalibrary/src/main/java/loitp/uizalibrary/app/activity/customviews/viewpager/placeholderview/_lib/placeholderview/listeners/ItemRemovedListener.java
+++
@@ -1,9 +0,0 @@
-package loitp.uizalibrary.app.activity.customviews.viewpager.placeholderview._lib.placeholderview.listeners;
-
-/**
- * Created by janisharali on 18/01/17.
- */
-
-public interface ItemRemovedListener {
-    void onItemRemoved(int count);
-}
