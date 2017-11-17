42c09081ca4098d6a416ab93de7ff1f141c89487
Loitp <loitp@pateco.vn>
2017-11-17 17:02:07 +0700

    update login screen

---
+++ demouiza/src/main/java/vn/loitp/app/uiza/player/FrmUizaVideoInfo.java
@@ -0,0 +1,33 @@
+package vn.loitp.app.uiza.player;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.loitp.app.base.BaseFragment;
+import vn.loitp.livestar.R;
+
+/**
+ * Created by www.muathu@gmail.com on 7/26/2017.
+ */
+
+public class FrmUizaVideoInfo extends BaseFragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.uiza_video_info_frm, container, false);
+        //TextView tv = (TextView) view.findViewById(R.id.tv);
+        return view;
+    }
+}
