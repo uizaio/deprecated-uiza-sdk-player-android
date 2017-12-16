8437b96aee72b0c452056f8e09370eba135b2178
Loitp <loitp@pateco.vn>
2017-12-16 20:29:35 +0700

    update

--- library/ui/src/main/java/com/uiza/player/ui/player/FrmUizaVideoInfo.java
+++ library/ui/src/main/java/com/uiza/player/ui/player/FrmUizaVideoInfo.java
@@ -9,24 +9,20 @@ import android.widget.TextView;
 
 import com.uiza.player.core.uiza.api.model.getentityinfo.Item;
 import com.uiza.player.ui.data.UizaData;
-import com.uiza.player.ui.data.UizaRepositoryObserver;
-import com.uiza.player.ui.data.UizaSubject;
 import com.uiza.player.ui.views.helper.InputModel;
 
 import io.uiza.sdk.ui.R;
 import vn.loitp.core.base.BaseFragment;
 import vn.loitp.core.utilities.LLog;
 import vn.loitp.core.utilities.LUIUtil;
-import vn.loitp.views.progressloadingview.avloadingindicatorview.lib.avi.AVLoadingIndicatorView;
 
 /**
  * Created by www.muathu@gmail.com on 7/26/2017.
  */
 
-public class FrmUizaVideoInfo extends BaseFragment implements UizaRepositoryObserver {
+public class FrmUizaVideoInfo extends BaseFragment {
     private final String TAG = getClass().getSimpleName();
-    private UizaSubject mUserDataRepository;
-    private AVLoadingIndicatorView avLoadingIndicatorView;
+    //private AVLoadingIndicatorView avLoadingIndicatorView;
     private TextView tvVideoName;
     private TextView tvVideoTime;
     private TextView tvVideoRate;
@@ -47,23 +43,15 @@ public class FrmUizaVideoInfo extends BaseFragment implements UizaRepositoryObse
     }
 
     @Override
-    public void onDestroyView() {
-        mUserDataRepository.removeObserver(this);
-        super.onDestroyView();
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LLog.d(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.uiza_video_info_frm, container, false);
-
-        mUserDataRepository = UizaData.getInstance();
-        mUserDataRepository.registerObserver(this);
 
         ScrollView scrollView = (ScrollView) view.findViewById(R.id.scroll_view);
         LUIUtil.setPullLikeIOSVertical(scrollView);
 
-        avLoadingIndicatorView = (AVLoadingIndicatorView) view.findViewById(R.id.avi);
-        avLoadingIndicatorView.smoothToShow();
+        //avLoadingIndicatorView = (AVLoadingIndicatorView) view.findViewById(R.id.avi);
+        //avLoadingIndicatorView.smoothToShow();
 
         tvVideoName = (TextView) view.findViewById(R.id.tv_video_name);
         tvVideoTime = (TextView) view.findViewById(R.id.tv_video_time);
@@ -74,66 +62,26 @@ public class FrmUizaVideoInfo extends BaseFragment implements UizaRepositoryObse
         tvVideoGenres = (TextView) view.findViewById(R.id.tv_video_genres);
         tvDebugJson = (TextView) view.findViewById(R.id.tv_debug_json);
 
+        mInputModel = UizaData.getInstance().getInputModel();
+        setup();
         return view;
     }
 
-
-    @Override
-    public void onInputModelChange(InputModel inputModel) {
-        if (inputModel == null) {
-            LLog.d(TAG, "onInputModelChange -> return");
+    private void setup() {
+        if (mInputModel == null) {
             return;
         }
-        this.mInputModel = inputModel;
-        if (mItem == null && !avLoadingIndicatorView.isShown()) {
+        if (mItem == null) {
             //getDetailEntity();
             if (mInputModel.getEntityInfo() != null) {
                 mItem = mInputModel.getEntityInfo().getItem();
                 updateUI();
             }
-            avLoadingIndicatorView.smoothToHide();
         }
     }
 
     private InputModel mInputModel;
     private Item mItem;
-
-    /*private void getDetailEntity() {
-        LLog.d(TAG, "getDetailEntity");
-        if (mInputModel == null) {
-            LLog.d(TAG, "mInputModel == null -> return");
-            return;
-        }
-        UizaService service = RestClient.createService(UizaService.class);
-        int entity;
-        try {
-            entity = Integer.parseInt(mInputModel.getEntityID());
-        } catch (Exception e) {
-            handleException("Exception " + e.toString());
-            return;
-        }
-        LLog.d(TAG, "entity " + entity);
-        subscribe(service.getDetailEntity(entity), new ApiSubscriber<DetailEntity>() {
-            @Override
-            public void onSuccess(DetailEntity detailEntity) {
-                Gson gson = new Gson();
-                LLog.d(TAG, "getDetailEntity onSuccess " + gson.toJson(detailEntity));
-                if (detailEntity != null) {
-                    mItem = detailEntity.getItem().get(0);
-                    updateUI();
-                } else {
-                    handleException("getDetailEntity Error");
-                }
-                avLoadingIndicatorView.smoothToHide();
-            }
-
-            @Override
-            public void onFail(Throwable e) {
-                avLoadingIndicatorView.smoothToHide();
-                handleException(e);
-            }
-        });
-    }*/
 
     private void updateUI() {
         String empty = "Empty";
