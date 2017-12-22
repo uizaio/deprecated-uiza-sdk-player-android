9184a13bd335f48235f5113fc1b2f62dfd15e2b6
Loitp <loitp@pateco.vn>
2017-12-22 13:57:26 +0700

    update fake link play

--- library/ui/src/main/java/com/uiza/player/ui/player/PlayerActivity.java
+++ library/ui/src/main/java/com/uiza/player/ui/player/PlayerActivity.java
@@ -209,8 +209,9 @@ public class PlayerActivity extends BaseActivity {
             @Override
             public void onSuccess(GetLinkPlay getLinkPlay) {
                 LLog.d(TAG, "getLinkPlay onSuccess " + gson.toJson(getLinkPlay));
-                //UizaData.getInstance().setLinkPlay("http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0");
-                UizaData.getInstance().setLinkPlay(getLinkPlay.getLinkplayMpd());
+                UizaData.getInstance().setLinkPlay("http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0");
+                //TODO
+                //UizaData.getInstance().setLinkPlay(getLinkPlay.getLinkplayMpd());
                 isGetLinkPlayDone = true;
                 init();
             }
