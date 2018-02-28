67ad5164e4f18a104371edb15a4fd760c72b8f4f
Loitp <loitp@pateco.vn>
2018-02-28 14:53:11 +0700

    set size surface view

--- demouiza/src/main/java/vn/loitp/app/uiza/search/SearchActivity.java
+++ demouiza/src/main/java/vn/loitp/app/uiza/search/SearchActivity.java
@@ -2,15 +2,55 @@ package vn.loitp.app.uiza.search;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import vn.loitp.core.base.BaseActivity;
 import vn.loitp.uiza.R;
 
-public class SearchActivity extends BaseActivity {
+public class SearchActivity extends BaseActivity implements View.OnClickListener {
+    private ImageView ivBack;
+    private ImageView ivClearText;
+    private EditText etSearch;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ivBack = (ImageView) findViewById(R.id.iv_back);
+        ivClearText = (ImageView) findViewById(R.id.iv_clear_text);
+        etSearch = (EditText) findViewById(R.id.et_search);
+        tv = (TextView) findViewById(R.id.tv);
+
+        ivBack.setOnClickListener(this);
+        ivClearText.setOnClickListener(this);
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().isEmpty()) {
+                    ivClearText.setVisibility(View.GONE);
+                    resetAllView();
+                } else {
+                    ivClearText.setVisibility(View.VISIBLE);
+                    search(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //do nothing
+            }
+        });
     }
 
     @Override
@@ -31,5 +71,28 @@ public class SearchActivity extends BaseActivity {
     @Override
     protected int setLayoutResourceId() {
         return R.layout.uiza_search_activity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_back:
+                onBackPressed();
+                break;
+            case R.id.iv_clear_text:
+                etSearch.setText("");
+                break;
+        }
+    }
+
+    private void search(String keyword) {
+        //TODO
+        tv.setText("Không tìm thấy kết quả nào cho\n" + keyword);
+        tv.setText(View.VISIBLE);
+    }
+
+    private void resetAllView() {
+        tv.setText("");
+        tv.setVisibility(View.GONE);
     }
 }
