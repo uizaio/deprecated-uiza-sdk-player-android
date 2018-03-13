deb678a4ad620c0b5f37fe480724b8f7af6b63b3
Loitp <loitp@pateco.vn>
2018-03-13 16:28:40 +0700

    update search

--- demouiza/src/main/java/vn/loitp/app/uiza/home/v2/FrmSearch.java
+++ demouiza/src/main/java/vn/loitp/app/uiza/home/v2/FrmSearch.java
@@ -8,16 +8,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 
 import io.uiza.sdk.ui.R;
 import vn.loitp.core.base.BaseFragment;
 import vn.loitp.core.utilities.LLog;
-import vn.loitp.data.EventBusData;
 
 /**
  * Created by www.muathu@gmail.com on 7/26/2017.
