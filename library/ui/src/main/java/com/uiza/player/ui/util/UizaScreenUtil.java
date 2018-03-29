d890a8200998090723b0d4d24a779c19543bc652
Loitp <loitp@pateco.vn>
2018-03-29 18:01:21 +0700

    fix tracking uiza

--- library/ui/src/main/java/com/uiza/player/ui/util/UizaScreenUtil.java
+++ library/ui/src/main/java/com/uiza/player/ui/util/UizaScreenUtil.java
@@ -16,9 +16,8 @@ import android.view.WindowManager;
 
 import com.uiza.player.ui.data.UizaData;
 
-import io.uiza.sdk.ui.R;
 import vn.loitp.core.base.BaseActivity;
-import vn.loitp.core.utilities.LScreenUtil;
+import vn.loitp.core.utilities.LLog;
 
 /**
  * File created on 8/31/2017.
@@ -27,6 +26,8 @@ import vn.loitp.core.utilities.LScreenUtil;
  */
 
 public class UizaScreenUtil {
+    private final static String TAG = UizaScreenUtil.class.getSimpleName();
+
     public int getStatusBarHeight(Context mContext) {
         int result = 0;
         int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
@@ -52,43 +53,6 @@ public class UizaScreenUtil {
             return result;
         }
         return 0;
-    }
-
-    public static void hideNavBar(View v) {
-        if (Build.VERSION.SDK_INT >= 19) {
-            //View v = getWindow().getDecorView();
-            v.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
-
-    public static void showNavigationBar(Activity activity) {
-        // set navigation bar status, remember to disable "setNavigationBarTintEnabled"
-        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        // This work only for android 4.4+
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(flags);
-
-            // Code below is to handle presses of Volume up or Volume down.
-            // Without this, after pressing volume buttons, the navigation bar will
-            // show up and won't hide
-            final View decorView = activity.getWindow().getDecorView();
-            decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-                @Override
-                public void onSystemUiVisibilityChange(int visibility) {
-                    if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                        decorView.setSystemUiVisibility(flags);
-                    }
-                }
-            });
-        }
     }
 
     public static int getScreenWidth() {
@@ -123,21 +87,90 @@ public class UizaScreenUtil {
         }
     }
 
-    //true: show status bar, hide navigation bar
-    //false: hide status bar, hide navigation bar
-    /*public static void updateUIStatusNavigationBar(Activity activity, boolean isShow) {
-        UizaScreenUtil.hideNavBar(activity.getWindow().getDecorView());
-        if (isShow) {
-            UizaScreenUtil.showStatusBar(activity);
-        } else {
-            UizaScreenUtil.hideStatusBar(activity);
-        }
-    }*/
-
-    public static void toggleFullscreen(Activity activity) {
+    /*public static void toggleFullscreen(Activity activity) {
         WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
         attrs.flags ^= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        //attrs.flags ^= WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
+        //attrs.flags ^= WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.SOFT_INPUT_IS_FORWARD_NAVIGATION;
         activity.getWindow().setAttributes(attrs);
+    }*/
+
+    /**
+     * toggles fullscreen mode
+     * <br/>
+     * REQUIRE: android:configChanges="orientation|screenSize"
+     * <pre>
+     * sample:
+     *     private boolean fullscreen;
+     *     ................
+     *     Activity activity = (Activity)context;
+     *     toggleFullscreen(activity, !fullscreen);
+     *     fullscreen = !fullscreen;
+     * </pre>
+     */
+    public static void toggleFullscreen(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            // The UI options currently enabled are represented by a bitfield.
+            // getSystemUiVisibility() gives us that bitfield.
+            int uiOptions = activity.getWindow().getDecorView().getSystemUiVisibility();
+            int newUiOptions = uiOptions;
+            boolean isImmersiveModeEnabled =
+                    ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+            if (isImmersiveModeEnabled) {
+                LLog.d(TAG, "Turning immersive mode mode off. ");
+            } else {
+                LLog.d(TAG, "Turning immersive mode mode on.");
+            }
+
+            // Navigation bar hiding:  Backwards compatible to ICS.
+            if (Build.VERSION.SDK_INT >= 14) {
+                newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+
+            // Status bar hiding: Backwards compatible to Jellybean
+            if (Build.VERSION.SDK_INT >= 16) {
+                newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+            }
+
+            // Immersive mode: Backward compatible to KitKat.
+            // Note that this flag doesn't do anything by itself, it only augments the behavior
+            // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+            // all three flags are being toggled together.
+            // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+            // Sticky immersive mode differs in that it makes the navigation and status bars
+            // semi-transparent, and the UI flag does not get cleared when the user interacts with
+            // the screen.
+            if (Build.VERSION.SDK_INT >= 18) {
+                newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+            activity.getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        } else {
+            // for android pre 11
+            WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+            if (UizaScreenUtil.isFullScreen(activity)) {
+                attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            } else {
+                attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            }
+            activity.getWindow().setAttributes(attrs);
+        }
+
+        /*try {
+            // hide actionbar
+            if (activity instanceof ActionBarActivity) {
+                if (fullscreen) ((ActionBarActivity) activity).getSupportActionBar().hide();
+                else ((ActionBarActivity) activity).getSupportActionBar().show();
+            } else if (Build.VERSION.SDK_INT >= 11) {
+                if (fullscreen) activity.getActionBar().hide();
+                else activity.getActionBar().show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        // set landscape
+        // if(fullscreen)  activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        // else activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
     }
 
     //rotate screen
@@ -145,9 +178,11 @@ public class UizaScreenUtil {
         if (isFullScreen) {
             ((Activity) context).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
             UizaData.getInstance().setLandscape(false);
+
         } else {
             ((Activity) context).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
             UizaData.getInstance().setLandscape(true);
+
         }
     }
 
