4f92526522ea50744f55928ee7d0f1cb3307951f
Loitp <loitp@pateco.vn>
2018-04-16 17:30:43 +0700

    update

--- corebase/src/main/java/vn/loitp/core/utilities/LKeyBoardUtil.java
+++ corebase/src/main/java/vn/loitp/core/utilities/LKeyBoardUtil.java
@@ -1,7 +1,9 @@
 package vn.loitp.core.utilities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by Loitp on 4/1/2017.
@@ -16,6 +18,12 @@ public class LKeyBoardUtil {
     public static void hide(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0); // hide
+    }
+
+    public static void hide(EditText editText) {
+        editText.setInputType(0);
+        InputMethodManager imm = (InputMethodManager) editText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
     }
 
     public static void toggle(Activity activity) {
